536fb3240294a699730c4411dc7a094cf1663971
Shiva Bajpai <shivabajpai2006@gmail.com>
2025-01-10 02:48:23 +0530

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,9 @@
+def even_or_odd():
+    print("Even or Odd Game")
+    number = int(input("Enter a number: "))
+    if number % 2 == 0:
+        print("It's Even!")
+    else:
+        print("It's Odd!")
+
+even_or_odd()
